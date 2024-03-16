cfc9a3cbeda207316b0356fe462bfe2b9b596382
Amanda Grimm <95390235+amandagrimm95@users.noreply.github.com>
2024-03-16 04:29:34 -0400

    updating portfolio images

--- src/components/portfolio/portfolioInfo.tsx
+++ src/components/portfolio/portfolioInfo.tsx
@@ -7,21 +7,23 @@ type portfolioInfoType = {
 }
 
 export const portfolioInfo: portfolioInfoType[] = [{
-    name: 'Family Medical Practice Site',
-    image: 'images/VillageDoctor.png',
-    link: 'http://www.thevillagedoctor.net',
-    tech: ['TypeScript', 'JavaScript', 'React', 'HTML5/CSS3', 'Mockplus'],
-    features: ['Static site', 'Select cpmponents easily editable by client', 'Rework of existing site, maintaining content and brand identity']
-},{
     name: 'Software Dev & Consulting Site',
-    image: 'images/GrimbarInt.png',
+    image: 'images/GrimbarInt2.png',
     link: 'https://www.grimbarinteractive.com',
     tech: ['TypeScript', 'JavaScript', 'React', 'HTML5/CSS3', 'NodeJS', 'MySQL', 'Mockplus'],
     features: ['Dynamic site', 'Email collection', 'Interactive crowd sourced "create your own adventure" game available for events']
-// },{
+},{
+    name: 'Family Medical Practice Site',
+    image: 'images/VillageDoctor2.png',
+    link: 'http://www.thevillagedoctor.net',
+    tech: ['TypeScript', 'JavaScript', 'React', 'HTML5/CSS3', 'Mockplus'],
+    features: ['Static site', 'Select cpmponents easily editable by client', 'Rework of existing site, maintaining content and brand identity']
+},
+// {
 //     name: 'Nutrition & Fitness Site',
 //     image: 'images/FitnessLexx.png',
 //     link: 'https://www.fitnesslexx.com',
 //     tech: ['Kajabi Website Builder'],
 //     features: ['Integration with client Kajabi Course Catalog', 'Email collection', 'Active Blog', 'Member sign-in and library']
-}]
+// }
+]
