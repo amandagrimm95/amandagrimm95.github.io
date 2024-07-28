4e5c0fe90744de1eeaa875a261dd80b54d8e9b16
Amanda Grimm <95390235+amandagrimm95@users.noreply.github.com>
2024-07-27 21:54:10 -0400

    commented out figma frame

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -9,7 +9,7 @@ export function Portfolio () {
     const iframeStyle = {
         border: "1px solid rgba(0, 0, 0, 0.1)",
         width: "600px",
-        height: "450px"
+        height: "450px",
     }
 
     function renderPortfolioItems() {
@@ -22,7 +22,7 @@ export function Portfolio () {
         <div id='portfolioSection'>
             <h2 className="section-title">Portfolio</h2>
             {renderPortfolioItems()}
-            {/* <iframe title="laptop-img" style={iframeStyle} src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FIuRGy8aRbw6p89uUnlFqpG%2FUntitled%3Fpage-id%3D0%253A1%26node-id%3D6%253A2%26viewport%3D529%252C402%252C0.48%26scaling%3Dscale-down"></iframe> */}
+            {/* <iframe style={iframeStyle} src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FTA25jetTifyUCoXe7JCw4M%2FStitch-Sculptor%3Fpage-id%3D0%253A1%26type%3Ddesign%26node-id%3D219-131%26viewport%3D-247%252C338%252C0.09%26t%3D09qDuPbGVi2pjZ72-1%26scaling%3Dscale-down%26starting-point-node-id%3D219%253A131%26mode%3Ddesign" allowFullScreen={true}></iframe> */}
         </div>
         
     )
